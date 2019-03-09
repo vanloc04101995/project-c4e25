2612f8d2d5575e5d3e923781bb6652240c2af78a
hanguyen31099 <ducha31099@gmail.com>
2019-03-10 02:53:01 +0700

    update

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ mlab.connect()
 def home():
     test = 1
     if request.method == "GET":
-        # get database
         sounds = []
         data = Sound.objects()
         for sound in data:
@@ -23,60 +22,31 @@ def home():
                 "loading": True,
             }
             sounds.append(item)
-        # print(sounds)
         return render_template("homepage-out.html", dataHtml=sounds,test=test)
-    elif request.method == "POST":
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        sounds = []
-        data = Sound.objects()
-        for sound in data:
-            item = {
-                "name": sound.name,
-                "link": sound.link,
-                "classIcon": sound.classIcon,
-                "playing": False,
-                "audio": None,
-            }
-            sounds.append(item)
-        print(sounds)
         user = Register.objects(username=username).first()
         if user != None:
             if user.password==password :
                 session["token"] = username
-                return render_template("homepage-in.html",dataHtml=sounds,userName=user)
+                session["logged"] = True
+                return redirect("/")
             else:
                 flash("Invalid password")
                 return redirect("/")
         else:
             flash("Invalid username")
             return redirect("/")
-    # elif request.method == "POST":
-    #     username= request.form["username"]
-    #     password=request.form["password"]
-    #     dateOfBirth = request.form["dateOfBirth"]
-    #     email = request.form["email"]
-    #     f_objects =  Register(username=username, password= password,dateOfBirth=dateOfBirth,email=email)
-    #     f_objects.save()
-    #     return redirect("/")
-# def signup():
-#     if request.method == "GET":
-#         return render_template("sign-up-form.html")
-#     elif request.method == "POST":
-#         username= request.form["username"]
-#         password=request.form["password"]
-#         dateOfBirth = request.form["dateOfBirth"]
-#         email = request.form["email"]
-#         f_objects =  Register(username=username, password= password,dateOfBirth=dateOfBirth,email=email)
-#         f_objects.save()
-#         return redirect("/")
-
-
 
 @app.route("/logout")
 def logout():
     if "token" in session:
         del session["token"]
+        session["logged"] = False
         return redirect("/")
     else:
         return redirect("/")
